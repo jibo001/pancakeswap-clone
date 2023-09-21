4651c4900baccc61304472c6a9e8109d42dd563e
Jeanice Jones <56509810+jibo001@users.noreply.github.com>
2023-09-21 15:23:08 +0800

    Initial commit

---
+++ src/hooks/useActiveChain.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+import { fromHex } from 'viem';
+
+/**
+ * @return 获取当前链的 chainId
+ */
+const useActiveChain = () => {
+  const [chainId, setChainId] = useState<number>();
+  useEffect(() => {
+
+    setChainId(fromHex(window.ethereum?.chainId ?? '0x0', 'number'));
+    if (window?.ethereum) {
+      window.ethereum.on('chainChanged', (currentChainId: `0x${string}`) => {
+        setChainId(fromHex(currentChainId, 'number'));
+      });
+    }
+  }, []);
+  return chainId;
+};
+
+export default useActiveChain
